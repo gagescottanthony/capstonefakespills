c26353e124ac577dc85323b8b7146b9b9ca68774
gagescottanthony <gagescottanthony@gmail.com>
2025-01-25 21:28:23 -0500

    data loader can navigate image set folders now (with local paths)

--- segmaps.py
+++ segmaps.py
@@ -1,7 +1,9 @@
 import argparse
+import os
 import matplotlib.pyplot as plt
 import numpy as np
 import cv2 as cv
+import string
 
 #Constants
 cropped_image_directory = "./cropped"
@@ -9,21 +11,25 @@ segment_map_directory = "./segmentmaps"
 
 def main():
     #Arg parsing from input.
-    arg_parser = argparse.ArgumentParser()
+    arg_parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
     arg_parser.add_argument("--verbose", "-v", required=False, default=False, action="store_true")
-    arg_parser.add_argument('--manifest', '-m', required=True, help='.txt manifest file for input data sets')
-
+    arg_parser.add_argument('--manifest', '-m', required=False, help='.txt manifest file for input data sets')
+    arg_parser.epilog = ("Manifest file requirements:\n"
+                         "lines beginning with number sign # are ignored for easier temporary edits and comments\n"
+                         "all images in a set must use same file extension :)\n"
+                         "entries are separated by line and formatted as \"NAME, .extension, number of digits in file number\" (space after comma not strictly necessary)")
     args = arg_parser.parse_args()
     manifest_file = args.manifest
     verbose = args.verbose
 
     #Manifest file provides the codenames of all the image sets we want to use (ex: ts1, ts2)
-    if(manifest_file == None):
-        print("No manifest file provided")
-        exit(1)
-    print("Using" + manifest_file + " as manifest file for input data.")
-
     image_sets = []
+    if(manifest_file == None):
+        print("No manifest file provided, exiting")
+        arg_parser.print_help()
+        exit(1)
+    print("Using " + manifest_file + " as manifest file for input data.")
+
     file = open(manifest_file, 'r')
     for line in file:
         line = line.strip()
@@ -40,6 +46,24 @@ def main():
         for item in image_sets:
             print(item)
 
+    for entry in image_sets:
+        setname = entry[0]
+        extension = entry[1]
+        n_digits = int(entry[2])
+        i = 0
+        while True:
+            filenum = str(i)
+            nextimage = cropped_image_directory + "/" + setname + "/" + setname + "-" + filenum.zfill(n_digits) + extension #script specific local pathing
+            nextpath = os.path.normpath(nextimage) #storing os specific changes (might be abspath)
+            if verbose: print("Opening:" + nextimage + ".", end="\t")
+            if os.path.isfile(nextpath):
+                if verbose: print("Success")
+                i = i + 1
+            else:
+                if verbose: print("Failed")
+                print("No more images found in set " + setname + ".")
+                if verbose: print("HELP: Check number of extension and number of digits in manifest entry if this is incorrect.")
+                break
 
 
 if __name__ == "__main__":
